ccbdbdff081054d48feb7e5d66709fbd3a174e2f
Alan Ho <alan.ho@contin-global.com>
2024-09-13 11:39:09 +0800

    feat(auth): 新增 login API

---
+++ src/lib/auth/api.ts
@@ -0,0 +1,19 @@
+/* Login */
+type login = {
+  email: string;
+  password: string;
+}
+
+export const login = async (user: login) => {
+  const res = await fetch(`/api/auth/login`, {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify(user)
+  })
+
+  const data = await res.json();
+
+  return data;
+}
