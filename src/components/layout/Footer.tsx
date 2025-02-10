0772227738ec400bf1131b4868434317892b7c59
Alan Ho <alan.ho@contin-global.com>
2025-02-10 10:18:37 +0800

    feat: 新增 Profile 頁面

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,16 +2,17 @@ import Link from 'next/link'
 import { CalendarHeart, UserRound } from 'lucide-react';
 
 const Footer: React.FC = () => {
+  
   return (
     <div className="flex justify-around bg-primary text-white text-center py-4">
-      <button className="flex flex-col items-center justify-center">
+      <Link href="/dashboard" className="flex flex-col items-center justify-center">
         <CalendarHeart />
         <small>Record</small>
-      </button>
-      <button className="flex flex-col items-center justify-center">
+      </Link>
+      <Link href="/profile" className="flex flex-col items-center justify-center">
         <UserRound />
         <small>Profile</small>
-      </button>
+      </Link>
     </div>
   );
 };
---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,52 @@
+import { useRouter } from "next/router";
+import Layout from "@/components/layout/Layout";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useUser } from "@/hooks/useUser";
+import { Button } from "@/components/ui/button";
+import { logout } from "@/lib/auth/api";
+import { clearAllStore } from "@/store/clear";
+
+const Profile = () => {
+    const router = useRouter();
+    const [user, setUser] = useUser();
+
+    const handlerLogout = async () => {
+        const res = await logout();
+        if(res.status === 200) {
+            clearAllStore(); 
+            await router.replace("/login");
+        }
+    }
+
+    return (
+        <div className="p-8">
+            <div className="flex justify-between items-center">
+                <Avatar className="w-24 h-24">
+                    <AvatarImage src={ user.avatarImg ?? "https://github.com/shadcn.png" } alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+
+                <ul className="flex flex-col gap-2">
+                    <li>名稱：{user.name}</li>
+                    <li>信箱：{user.email}</li>
+                    <li>密碼：********</li>
+                </ul>
+            </div>
+            <p className="my-6">下次諮詢日期：{user.nextConsultation}</p>
+            <Button
+                onClick={handlerLogout}
+                className="w-full mt"
+            >
+                Log Out
+            </Button>
+        </div>
+    );
+};
+
+Profile.getLayout = (page: React.ReactNode) => (
+    <Layout>
+      { page }
+    </Layout>
+);
+
+export default Profile;
